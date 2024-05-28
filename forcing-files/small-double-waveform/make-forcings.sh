b94067686244f002923990a2b9fec86e07bdfc9a
engeir <engeir@pm.me>
2024-05-28 14:28:46 +0200

    feat(forcing): create experiments for small, double eruption

--- forcing-files/small-double-waveform/make-forcings.sh
+++ forcing-files/small-double-waveform/make-forcings.sh
@@ -10,9 +10,27 @@ if [[ $(volcano-cooking --version) != "volcano-cooking, version 0.12.2" ]]; then
     exit 1
 fi
 
-# TT, 2 year, ens 2
+# TT, 2 year, ens 1
 volcano-cooking --file "./tt-2year-ens1-forcing.json"
 volcano-cooking --run-ncl
 volcano-cooking --package-last
 rm -rf source-files-tt-2year-ens1
 mv source-files source-files-tt-2year-ens1
+# TT, 2 year, ens 3
+volcano-cooking --file "./tt-2year-ens3-forcing.json"
+volcano-cooking --run-ncl
+volcano-cooking --package-last
+rm -rf source-files-tt-2year-ens3
+mv source-files source-files-tt-2year-ens3
+# TT, 4 year, ens 1
+volcano-cooking --file "./tt-4year-ens1-forcing.json"
+volcano-cooking --run-ncl
+volcano-cooking --package-last
+rm -rf source-files-tt-4year-ens1
+mv source-files source-files-tt-4year-ens1
+# TT, 4 year, ens 3
+volcano-cooking --file "./tt-4year-ens3-forcing.json"
+volcano-cooking --run-ncl
+volcano-cooking --package-last
+rm -rf source-files-tt-4year-ens3
+mv source-files source-files-tt-4year-ens3
